712e855c8dc6a5e6a7cf29bbf86022a9f9af2b2f
Fries <152449510+friesisverycool@users.noreply.github.com>
2023-12-01 18:15:44 -0500

    Delete api directory

--- api/getServiceHours.js
+++
@@ -1,9 +0,0 @@
-let data = [];
-
-export default async function handler(req, res) {
-    if (req.method === 'GET') {
-        res.status(200).json(data);
-    } else {
-        res.status(405).json({ error: 'Method Not Allowed' });
-    }
-}
